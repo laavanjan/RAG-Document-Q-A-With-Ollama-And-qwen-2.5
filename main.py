6b31290ea2509ce8cb2cd46277c7152659953e9e
laavanjan <130158996+laavanjan@users.noreply.github.com>
2025-02-15 23:50:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,93 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+
+
+from dotenv import load_dotenv
+load_dotenv()
+## load the GROQ API Key
+os.environ['GROQ_API_KEY']=os.getenv("GROQ_KEY")
+
+groq_api_key=os.getenv("GROQ_KEY")
+
+llm=ChatGroq(groq_api_key=groq_api_key,model_name="qwen-2.5-32b")
+
+prompt=ChatPromptTemplate.from_template(
+    """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate respone based on the question
+    <context>
+    {context}
+    <context>
+    Question:{input}
+
+    """
+
+)
+
+def create_vector_embedding():
+    if "vectors" not in st.session_state:
+        st.session_state.embeddings=OllamaEmbeddings(model="llama3.2")
+        st.session_state.loader=PyPDFDirectoryLoader("research_papers") ## Data Ingestion step
+        st.session_state.docs=st.session_state.loader.load() ## Document Loading
+        st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)
+        st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:100])
+        st.session_state.vectors=FAISS.from_documents(st.session_state.final_documents,st.session_state.embeddings)
+st.title("RAG Document Q&A With Ollama And qwen-2.5")
+
+user_prompt=st.text_input("Enter your query from the research paper")
+
+if st.button("Document Embedding"):
+    create_vector_embedding()
+    st.write("Vector Database is ready")
+
+import time
+
+if user_prompt:
+    document_chain=create_stuff_documents_chain(llm,prompt)
+    retriever=st.session_state.vectors.as_retriever()
+    retrieval_chain=create_retrieval_chain(retriever,document_chain)
+
+    start=time.process_time()
+    response=retrieval_chain.invoke({'input':user_prompt})
+    print(f"Response time :{time.process_time()-start}")
+
+    st.write(response['answer'])
+
+    ## With a streamlit expander
+    with st.expander("Document similarity Search"):
+        for i,doc in enumerate(response['context']):
+            st.write(doc.page_content)
+            st.write('------------------------')
+
+
+custom_footer = """
+    <style>
+    .footer {
+        position: scroll;
+        bottom: 0;
+        width: 100%;
+        background-color: black;
+        text-align: center;
+        padding: 10px;
+        font-size: 14px;
+        color: white;
+        border:2px solid white;
+        border-radius:10px;
+    }
+    </style>
+    <div class="footer">
+        Developed by <b>Laavanjan</b> | © Faculty of IT B22
+    </div>
+"""
+st.markdown(custom_footer, unsafe_allow_html=True)
+
+
+
